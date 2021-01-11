0a301c6ae3eff407f8c82efcec83e999a0e17667
Brandon Lusk <Brandon Lusk>
2021-01-10 19:53:28 -0500

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faBoxes, faHome, faTicketAlt, faUsers } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ticket-app';
+  faTicketAlt = faTicketAlt;
+  faUsers = faUsers;
+  faBoxes = faBoxes;
 }
